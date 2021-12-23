57559c2863bd61cb9bf07a11080358e91014158a
SakaTaku2 <94817675+SakaTaku2@users.noreply.github.com>
2021-12-23 15:35:52 +0900

    Delete .myled.ko.cmd

--- .myled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/git/robosys_report1/myled.ko := ld -r  -EL  -maarch64elf  --build-id  -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds -o /home/ubuntu/git/robosys_report1/myled.ko /home/ubuntu/git/robosys_report1/myled.o /home/ubuntu/git/robosys_report1/myled.mod.o;  true
