41239f765f7be498bc80ccae0d86907933515528
SakaTaku2 <94817675+SakaTaku2@users.noreply.github.com>
2021-12-28 19:42:03 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -20,12 +20,6 @@ static struct cdev cdv;
 static struct class *cls = NULL;
 static volatile u32 *gpio_base = NULL; //アドレスをマッピングするための配列をグローバルで定義する
 
-char x;
-char y;
-
-x = 'KERN_INFO';
-y = 'KERN_ERR';
-
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos){
 	char c; //読み込んだ字を入れる変数
 	if(copy_from_user(&c,buf,sizeof(char)))
@@ -38,7 +32,7 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 		gpio_base[7] = 1 << 25;
 	}
 
-	printk(x "receive %c\n", c);
+	printk(KERN_INFO "receive %c\n", c);
 	return 1; //読み込んだ文字数を返す(この場合はダミーの１)
 }
 
@@ -46,7 +40,7 @@ static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* po
 	int size = 0;
 	char sushi[] ={'s', 'u', 's', 'h', 'i', 0x0A}; //寿司の絵文字のバイナリ
 	if(copy_to_user(buf+size,(const char *)sushi, sizeof(sushi))){
-		printk(x "sushi : copy_to_user failed\n");
+		printk(KERN_INFO "sushi : copy_to_user failed\n");
 		return -EFAULT;
 	}
 	size += sizeof(sushi);
@@ -67,18 +61,18 @@ static int __init init_mod(void){ //カーネルモジュール
 		return retval;
 	}
 
-	printk(x "%s is locaded. major:%d\n",__FILE__,MAJOR(dev));
+	printk(KERN_INFO "%s is locaded. major:%d\n",__FILE__,MAJOR(dev));
 	
 	cdev_init(&cdv, &led_fops);
  	retval = cdev_add(&cdv, dev, 1);
 	if(retval < 0){
-		printk(y "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
 		return retval;
 	}
 	
 	cls = class_create(THIS_MODULE,"myled");
 	if(IS_ERR(cls)){
-		printk(y"class_create failed.");
+		printk(KERN_ERR"class_create failed.");
 		return PTR_ERR(cls);
 	}
 	device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
@@ -99,7 +93,7 @@ static void __exit cleanup_mod(void){
 	device_destroy(cls, dev);
 	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
-	printk(x "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
 
 module_init(init_mod); //マクロで関数を登録
