168064c62ee231283eb444f0355f9ae153d6266b
SakaTaku2 <94817675+SakaTaku2@users.noreply.github.com>
2022-01-14 14:53:55 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -38,9 +38,9 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos){
 	int size = 0;
-	char sushi[] ={'s', 'u', 's', 'h', 'i', 0x0A}; //寿司の絵文字のバイナリ
+	char sunobo-[] ={'s', 'u', 'n', 'o', 'b', 'o', '-', 0x0A}; //寿司の絵文字のバイナリ
 	if(copy_to_user(buf+size,(const char *)sushi, sizeof(sushi))){
-		printk(KERN_INFO "sushi : copy_to_user failed\n");
+		printk(KERN_INFO "sunobo- : copy_to_user failed\n");
 		return -EFAULT;
 	}
 	size += sizeof(sushi);
